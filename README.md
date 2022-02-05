2bff76cadabbb929e157b88664dfa1d6be8c254e
Matej Pacan <matej@matejpacan.com>
2022-02-05 17:07:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<p align="center">
+  Do you want to learn Java the fun way? I've made a training for you!
+  <a href="https://mineacademy.org/project-orion">
+    <img src="https://i.imgur.com/OJuN0qP.png" />
+  </a>
+</p>
+
+# MavenProgram
+A sample template for creating standalone Java programs.
+
+By default we compile for Java 8, which can easily be changed in the sample pom.xml to any Java version, including the latest one.
---
+++ src/main/java/org/mineacademy/mavenprogram/MavenProgram.java
@@ -0,0 +1,25 @@
+package org.mineacademy.mavenprogram;
+
+import javax.swing.JOptionPane;
+
+/**
+ * The main program's class.
+ */
+public class MavenProgram {
+
+	/**
+	 * The main program's method. Every Java program has this method.
+	 * Your entire program starts here.
+	 *
+	 * Example: When you start your Minecraft, it too uses this method
+	 * to initialise everything and make sure the program then runs
+	 * indefinitely until you close it.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Simple method to show a closeable dialog
+		JOptionPane.showMessageDialog(null, "Java is fun");
+	}
+}
